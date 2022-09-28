829183ca73a6a819f6f679ebf689651100e471b9
bewld-devs <bewld.devs@gmail.com>
2022-09-28 12:37:36 +0300

    pushing to heroku

--- devs_talk_api_app/views/registration.py
+++ devs_talk_api_app/views/registration.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def register(request):
-    return HttpResponse("Registration Here")
+    return HttpResponse("Registration Coming")
