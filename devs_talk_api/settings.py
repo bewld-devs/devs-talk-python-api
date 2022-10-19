bc4f72739038277f24a1f4b8a6eb174fc578bc6a
bewld-devs <bewld.devs@gmail.com>
2022-10-20 02:14:42 +0300

    alter settings

--- devs_talk_api/settings.py
+++ devs_talk_api/settings.py
@@ -49,6 +49,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:4200",
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:9000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:5500",
