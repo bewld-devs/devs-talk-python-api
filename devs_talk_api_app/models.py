20832686ee5834b844f766cbe87005dd090c437f
bewld-devs <bewld.devs@gmail.com>
2022-11-11 23:45:29 +0300

    alter follow class

--- devs_talk_api_app/models.py
+++ devs_talk_api_app/models.py
@@ -158,7 +158,7 @@ def update_user_profile(sender, instance, created, **kwargs):
 
 
 class Follow(models.Model):
-    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='followers')
+    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='follower')
     following = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='following')
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
